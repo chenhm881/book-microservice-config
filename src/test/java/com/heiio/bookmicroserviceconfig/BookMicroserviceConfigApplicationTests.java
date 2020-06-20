a82d891b08e91e9060e0ae5372411359fb7dbfdc
huiming chen <v-huchen@expedia.com>
2020-06-20 23:47:33 +0800

    init commit

---
+++ src/test/java/com/heiio/bookmicroserviceconfig/BookMicroserviceConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heiio.bookmicroserviceconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMicroserviceConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
